d19e71fe53e30ac2fcbf25b7c4df16b2d7ecf880
KarynaStep <stepanova.carina98@gmail.com>
2023-07-19 16:34:41 +0300

    add v - 2

--- index4.js
+++ index4.js
@@ -12,15 +12,29 @@ console.log(checkValueInArray(array, 1));
 // Умова 2
 // Написати функцію, яка приймає масив з числами і повертає true, якщо в масиві є два однакових числа поспіль, інакше повертає false. Якщо масив порожній також повертає false.
 
-array = [1, 23, 45, 76, 78, 98, 98, 33, 442, 23, 20, 22]
+// в - 1
+array2 = [1, 23, 45, 45, 76, 78, 98, 33, 442, 23, 20, 22]
 
-function checkRepeatNumberRow(array) {
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] === array[index + 1]) {
+function checkRepeatNumberRow(array2) {
+  for (let index = 0; index < array2.length; index++) {
+    if (array2[index] === array2[index + 1]) {
       return true;
     } 
   }
   return false;
 }
+console.log(checkRepeatNumberRow(array2));
 
-console.log(checkRepeatNumberRow(array));
+// в - 2
+array3 = [1, 23, 45, 76, 78, 98, 33, 442, 23, 20, 22, 22]
+
+function checkRepeatNumberRow(array3) {
+  for (let index = 0; index < array3.length; index++) {
+    if (array3.indexOf(array3[index]) === array3.indexOf(array3[index + 1])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(checkRepeatNumberRow(array3));
